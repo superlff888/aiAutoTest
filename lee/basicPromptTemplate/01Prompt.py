4fbb9ee49e7ee68bc2e1c5c909b3d0277150473a
lee <1310157572@qq.com>
2026-04-14 01:33:38 +0800

    提交01Prompt.py

--- lee/basicPromptTemplate/01Prompt.py
+++ lee/basicPromptTemplate/01Prompt.py
@@ -1,3 +1,12 @@
+# !/usr/bin/env python3,# -*- coding: utf-8 -*-
+# --------------------------------------------
+# @FilePath    : \aiAutoTest\lee\basicPromptTemplate\01Prompt.py
+# @Author      : Lee大侠
+# @Desc        : 这是一个AI测试项目
+# @CreateTime  : 2026/04/14 01:05
+# @UpdateTime  : 2026/04/14 01:25
+# Copyright (c) 2026 Lee大侠. All rights reserved.
+# ========================================================
 # !/usr/bin/env python3,
 # # -*- coding: utf-8 -*-
 # --------------------------------------------
@@ -8,8 +17,6 @@
 # @UpdateTime  : 2026/04/13 13:35
 # Copyright (c) 2026 Lee大侠. All rights reserved.
 # ========================================================
-# !/usr/bin/env python3,# -*- coding: utf-8 -*-
-# --------------------------------------------
 
 
 import dotenv
@@ -19,7 +26,7 @@ from langchain_openai import ChatOpenAI
 
 dotenv.load_dotenv()
 llm = ChatOpenAI(
-    model_name=os.getenv("MODEL_NAME_QWEN2"),
+    model_name=os.getenv("MODEL_NAME_QWEN3"),
     base_url=os.getenv("BASE_URL"),
     api_key=os.getenv("API_KEY"),
 )
