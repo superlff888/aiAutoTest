32c23053137ef029b73faccd92f8443e14fd6049
lee <1310157572@qq.com>
2026-04-15 22:09:32 +0800

    fix: 修复大模型交互的第一个程序的错误

--- lee/01helloWorld/02langchain对大模型的统一封装.py
+++ lee/01helloWorld/02langchain对大模型的统一封装.py
@@ -1,32 +1,32 @@
 # !/usr/bin/env python3,# -*- coding: utf-8 -*-
 # --------------------------------------------
-# @FilePath    : \aiAutoTest\lee\01helloWorld\01大模型交互的第一个程序.py
+# @FilePath    : \aiAutoTest\lee\01helloWorld\02langchain对大模型的统一封装.py
 # @Author      : Lee大侠
 # @Desc        : 这是一个AI测试项目
-# @CreateTime  : 2026/04/15 21:44
+# @CreateTime  : 2026/04/15 22:03
 # @UpdateTime  : 2026/04/15 22:05
 # Copyright (c) 2026 Lee大侠. All rights reserved.
 # ========================================================
 
 
-
 import os
 import dotenv
-from langchain_openai import ChatOpenAI
-
+from langchain.chat_models import init_chat_model
 
 # 加载环境变量文件
 dotenv.load_dotenv()
 
 
-llm = ChatOpenAI(
+llm = init_chat_model(
     model = os.getenv("OPENAI_MODEL"),
     api_key = os.getenv("OPENAI_API_KEY"),
-    base_url = os.getenv("OPENAI_BASE_URL")
+    base_url = os.getenv("OPENAI_BASE_URL"),
+    model_provider = "openai"
     )
 
-response = llm.invoke("你好")
-print(response.content)
-
-for chunk in llm.stream("你好，你能在测试领域做什么？"):
+for chunk in llm.stream("你好"):
     print(chunk.content, end="")
+
+
+
+
