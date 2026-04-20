63a785a31eb0c5f9b857f564c523dfee26f72115
lee <1310157572@qq.com>
2026-04-20 22:52:40 +0800

    feat: Refactor langchain framework with new agent implementation and output parsers
    - Removed outdated `leeMCPServer.py` file.
    - Added new agent implementation in `01创建第一个智能体.py` for generating test cases from requirements.
    - Introduced basic model configuration in `02模型的基本配置.py` for LLM initialization.
    - Created output parsers for comma-separated lists, JSON, and Pydantic models in `leeCommaSeparatedListOutputParser.py`, `leeJsonOutputParser.py`, and `leePydanticOutputParser.py`.
    - Implemented runnable branches and maps in `leeRunnableBranch.py`, `leeRunnableMap.py`, and `leeRunnableLambda.py` for handling different model outputs.
    - Developed function calling capabilities with external tools in `lee_function_calling` directory.
    - Added examples demonstrating LLM execution methods in `llm执行的三种方式.py`.

---
+++ lee/03langchain框架/02模型的基本配置.py
@@ -0,0 +1,31 @@
+
+
+import os
+import dotenv
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+
+# 加载.env文件中的环境变量
+dotenv.load_dotenv()
+
+LLM = ChatOpenAI(
+    model=os.getenv('OPENAI_MODEL'),
+    base_url=os.getenv('OPENAI_BASE_URL'),
+    api_key=os.getenv('OPENAI_API_KEY'),
+    temperature=0.9,
+    max_retries=6,
+    max_tokens=2048
+)
+
+
+res = LLM.batch([
+    "帮我用python写一个pytest+requests的接口测试用例的dome,要求10行以内",
+    "请用python写一个爬虫程序，爬取百度首页的源码,10行以内"
+    ])
+
+for item in res:
+    print(f"流式批处理：\n{item[1].content}\n\n")
+
+
+
